ce3e1e2a1d5c08dc2d7d1834c0d30a15df0ad094
Antxnio19 <jesustrejo7914@gmail.com>
2024-05-17 23:14:38 -0400

    question from top 75

---
+++ Kids_With_Greates_Number_Candies.java
@@ -0,0 +1,19 @@
+public class Kids_With_Greates_Number_Candies {
+    
+        public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+            int maxCandies = 0;
+    
+            for (int candy : candies){
+                maxCandies = Math.max(candy, maxCandies);
+            }
+    
+            List<Boolean> result = new ArrayList<>();
+    
+            for(int candy: candies){
+                result.add(candy + extraCandies >= maxCandies);
+            }
+    
+            return result;
+        }
+    }
+
